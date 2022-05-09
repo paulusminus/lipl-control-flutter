b3d585c5cd431fa688ead0d06eecf6d0d66ab7ad
Paul Min <info@paulmin.nl>
2022-05-09 08:22:50 +0200

    Added web version

--- build_deploy_web.sh
+++ build_deploy_web.sh
@@ -2,4 +2,5 @@
 
 flutter pub get
 flutter build web --release --source-maps
+ssh paul@www.paulmin.nl 'rm -rf /var/www/html/lipl.paulmin.nl/*'
 rsync -avz build/web/* paul@www.paulmin.nl:/var/www/html/lipl.paulmin.nl/
